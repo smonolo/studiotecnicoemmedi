be89882270bdac7c47e6ee601d36027e5550e146
s.monolo <lu3mtex@gmail.com>
2022-09-30 17:23:31 +0200

    add basic config

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { Fragment } from "react";
+
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <Fragment>
+            <Head>
+                <meta charSet='utf-8' />
+                <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+                <title>Studio Tecnico Immobiliare EmmeDì</title>
+            </Head>
+            <Component {...pageProps} />
+        </Fragment>
+    );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div className='w-full h-screen flex justify-center items-center font-sans'>
+            <div>
+                <div className='text-3xl font-semibold text-slate-700'>Studio Tecnico Immobiliare EmmeDì</div>
+                <div className='mt-2'>
+                    <div className='text-1xl font-medium text-slate-600 text-center'>Sito in manutenzione.</div>
+                </div>
+            </div>
+        </div>
+    );
+}
